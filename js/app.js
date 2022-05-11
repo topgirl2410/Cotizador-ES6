836955fd9c1b02739020b0dab658baf9c670ba0f
topgirl2410 <maria.topgirl82@gmail.com>
2022-05-11 17:32:19 +0200

    creando la validacion del formulario

--- js/app.js
+++ js/app.js
@@ -27,16 +27,44 @@ UI.prototype.llenarOpciones = () => {
     }
 }
 
-
 // Instanciar UI
 
 const ui = new UI();
-
-console.log(ui)
-
 
 // Event Listener
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones(); // Llena el select con los años...
 })
+
+/************************************** Validar formulario *********************************************************************************************** */
+
+eventListeners();
+
+function eventListeners() {
+    const formulario = document.querySelector('#cotizar-seguro');
+
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+    e.preventDefault();
+
+    // Leer la marca seleccionada 
+    const marca = document.querySelector('#marca').value;
+
+    // Leer el año seleccionado
+    const year = document.querySelector('#year').value;
+
+    console.log(year)
+
+    // Leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    if (marca === '' || year === '' || tipo === '') {
+        console.log('no paso la validación')
+    }else {
+        console.log('si paso la validacion')
+    }
+
+}
